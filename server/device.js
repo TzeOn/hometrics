292903865b1f6227bf73d6dcfb17f39740ab7ebe
Lee Donovan <lee.donovan2303@gmail.com>
2020-01-28 19:51:24 +0000

    Added more functionality

--- server/device.js
+++ server/device.js
@@ -2,12 +2,28 @@ const express = require("express"),
       router = express.Router(),
       database = require("./database");
 
-router.post("/energyUsage", (request, response) => {
+router.post("/deviceUsage", (request, response) => {
     let x = request.body.id;
-    let sql = `SELECT SUM(ENERGY) FROM deviceActivity WHERE id= "${x}"`;
+    let sql = `SELECT SUM(Energy) FROM deviceActivity WHERE device = "${x}"`;
+    let energy = database.query(sql);
+    console.log(energy);
+    if(energy > 1){
+        response.json({"message": "ok"});
+    }else{
+        response.json({"message": "invalid id"});
+    }
+});
+
+
+router.post("/totalEnergyUse", (request, response) => {
+    let sql = `SELECT SUM(ENERGY) FROM deviceActivity`;
     let energy = database.query(sql);
 
-    console.log(energy);
+    if(energy == 1){
+        response.json({"message": "ok"});
+    }else{
+        response.json({"message": "error"});
+    }
 });
 
 module.exports = router;
