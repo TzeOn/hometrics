3f3e7a533aa605725ea195e19a6cd3df80d1d44f
Lee Donovan <lee.donovan2303@gmail.com>
2020-03-07 18:09:14 +0000

    Apparently i made a change to main.js

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ const express = require("express"),
 const port = process.env.PORT || 5000,
       app = express();
 
+
 app.listen(port, () => console.log(`Server up and running - listening for requests on port ${port}.`));
 
 app.use(cors()); 
@@ -32,3 +33,4 @@ app.use("/comfort", comfort);
 app.get("/", (request, response) => {
     console.log("Ping!");
 });
+
