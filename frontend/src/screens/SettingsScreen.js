3b46d61d1eff5d1a51410160d6eeb852920a600b
Yaseen Jamal <myj2@hw.ac.uk>
2020-03-08 23:19:40 +0000

    Update SettingsScreen.js

--- frontend/src/screens/SettingsScreen.js
+++ frontend/src/screens/SettingsScreen.js
@@ -1,12 +1,45 @@
 import React, { useState, Component } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, Image, Button, Dimensions } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, Image, Button, Dimensions, AsyncStorage, ActivityIndicator } from 'react-native';
 import { Card } from "react-native-elements"; 
 import { ScrollView } from 'react-native-gesture-handler';
 const trueHeight = Dimensions.get('window').height *.92;
+const api = require("../api").url; 
+
 
 export default class SettingsScreen extends Component {
     constructor(props) { 
         super(props); 
+        this.state = { 
+            loading: true
+        }
+    }
+
+    componentWillMount() { 
+        this.getData(); 
+    }
+
+    getData() { 
+        AsyncStorage.getItem("credentials").then(credentials => {
+            var emailAddress = JSON.parse(credentials).emailAddress; 
+
+
+            fetch(`${api}/user/type`, {
+                method: "POST", 
+                "headers": {
+                  Accept: "application/json", 
+                  "Content-Type": "application/json"
+                }, 
+                body: JSON.stringify({
+                  "emailAddress": emailAddress
+                })
+              }).then(response => response.json()).then(response => {console.log(response); this.setState({type: response.type, loading: false})
+              });         
+
+
+
+        }); 
+           
+    
     }
 
     render() {  
@@ -48,6 +81,16 @@ export default class SettingsScreen extends Component {
 
         return (
             <ScrollView contentContainerStyle={styles.container}>
+
+                {
+                    this.state.loading && 
+                    <ActivityIndicator size="large"/>
+                }
+
+                {!this.state.loading &&
+
+
+                
             <View style={styles.container}>
                 <View style={{flexDirection: 'row'}}>
                     <TouchableOpacity 
@@ -55,20 +98,30 @@ export default class SettingsScreen extends Component {
                     onPress={() => this.props.navigation.navigate('Settings')}>
                         <Text style={{borderBottomColor:'#41B3A3', borderBottomWidth:2, fontSize:20, color:'black', fontWeight:'600'}}>User Details</Text>
                     </TouchableOpacity>
-    
+     
+                    {this.state.type == "admin" &&
+
                     <TouchableOpacity 
                     style={styles.tabs}
                     onPress={() => this.props.navigation.navigate("ManageUsers")}>
                         <Text style={styles.textStyle}>User Management</Text>
                     </TouchableOpacity>
+    }
 
+    {this.state.type == "admin" &&
                     <TouchableOpacity 
                     style={styles.tabs}
                     onPress={() => this.props.navigation.navigate('Pending')}>
                         <Text style={styles.textStyle}>Requests</Text>
                     </TouchableOpacity>
+    }
+
+                
+
+
                 </View>
             </View>
+    }
             </ScrollView>
         );
     }
