a40b055538dc98ee49f8d0a4d68695b497535b93
TzeOn <tzevincent@gmail.com>
2020-04-07 16:41:47 +0100

    Styled energy screen

--- frontend/src/screens/EnergyScreen.js
+++ frontend/src/screens/EnergyScreen.js
@@ -6,6 +6,7 @@ import { ScrollView } from 'react-native-gesture-handler';
 const api = require("../api").url;
 const trueHeight = Dimensions.get('window').height * 1.5;
 const graphHeight = Dimensions.get('window').height * .4;
+const trueWidth = Dimensions.get('window').width *.8;
 
 export default class EnergyScreen extends Component {
     constructor(props) { 
@@ -13,9 +14,6 @@ export default class EnergyScreen extends Component {
         this.state = { 
             loading: true, 
             selectedValue: "monthly"
-        
-       
-
         }
     }
 
@@ -24,7 +22,6 @@ export default class EnergyScreen extends Component {
         var emailAddress; 
       
     AsyncStorage.getItem("credentials").then(credentials => {
-
 
         emailAddress = JSON.parse(credentials).emailAddress; 
 
@@ -42,11 +39,9 @@ export default class EnergyScreen extends Component {
               monthly: response.monthly, 
               yearly: response.yearly,
               filter: response.monthly 
-
-            
+           
             })}).then(() => {
                 
-
                 fetch(`http://localhost:5000/device/scoreboard`, {
                     method: "POST", 
                     "headers": {
@@ -77,38 +72,22 @@ export default class EnergyScreen extends Component {
                         console.log(this.state); 
 
                       })
-                    
-                    
                   }); 
-               
-
-
             }); 
-        
-
-
-     
-
     }); 
 }
-
-
 
     componentWillMount() { 
         this.getData(); 
         
     }
 
-   
-
     render() {
         const styles = StyleSheet.create({
             container: {
                 flex:1,
                 backgroundColor: '#E5FCFF',
-                alignItems: 'center',
-                justifyContent: "space-between",
-                height: trueHeight
+                height: trueHeight,
             },
             textStyle: {
                 color: 'black',
@@ -116,48 +95,42 @@ export default class EnergyScreen extends Component {
                 paddingTop:10,
                 textAlign: 'center'
             }
-         });
-         
+         });       
 
-        return (
-          
-                <View>
+        return (         
+                <View style={styles.container}>
 
                     {this.state.loading && 
-
-                            <View>
-                                <ActivityIndicator size="large"/>
-                            </View>
-                    
+                                <ActivityIndicator size="large"/>                 
                     }
 
                     {!this.state.loading && 
 
-                    <View>
+                    <View style={{flex:1, alignItems:'center'}}>
 
-                        <Text>
+                        <Text style={{textAlign:'center', fontSize:20, fontWeight:'500'}}>
                             Personal Statistics
                         </Text>
 
                         <Picker
-        selectedValue={this.state.selectedValue}
-        style={{ height: 50, width: 150 }}
-        onValueChange={(itemValue, itemIndex) => {
-            this.setState({selectedValue: itemValue}); 
-            if (itemValue === "weekly") 
-                this.setState({"filter": this.state.weekly}); 
-            else if (itemValue === "monthly")
-                this.setState({"filter": this.state.monthly}); 
-            else if (itemValue === "yearly")
-                this.setState({"filter": this.state.yearly}); 
+                          selectedValue={this.state.selectedValue}
+                          style={{ height: 50, width: 150, alignSelf:'center', padding:3, backgroundColor:'#ccfaff', color:'black', textAlign:'center', fontWeight:400 }}
+                          onValueChange={(itemValue, itemIndex) => {
+                              this.setState({selectedValue: itemValue}); 
+                              if (itemValue === "weekly") 
+                                  this.setState({"filter": this.state.weekly}); 
+                              else if (itemValue === "monthly")
+                                  this.setState({"filter": this.state.monthly}); 
+                              else if (itemValue === "yearly")
+                                  this.setState({"filter": this.state.yearly}); 
 
-        }}
-      >
-           <Picker.Item label="Monthly" value="monthly" />
-        <Picker.Item label="Weekly" value="weekly" />
-       
-        <Picker.Item label="Yearly" value="yearly" />
-      </Picker>
+                          }}>
+                        
+                          <Picker.Item label="Monthly" value="monthly" />
+                        <Picker.Item label="Weekly" value="weekly" />
+                      
+                        <Picker.Item label="Yearly" value="yearly" />
+                      </Picker>
 
                         <PureChart 
                         data={this.state.filter} 
@@ -166,43 +139,30 @@ export default class EnergyScreen extends Component {
                         height={200}
                         type="line"/>
 
-
-                      
-
-                    <Card title="Scoreboard">
+                    <View style={{flex:1, width:trueWidth}}>
+                    <Card title="Scoreboard" containerStyle={{backgroundColor:'#ccfaff'}} style={{flex:1, alignItems:'center'}}>
                     {
-    this.state.scoreboard.map((item, i) => (
-      <ListItem
- 
-     
-        title={item.emailAddress + " #"+(i+1)}
-        subtitle={item.value + "Units of energy"}
-        bottomDivider
-        chevron
- 
-      />
-    ))
-  }
+                      this.state.scoreboard.map((item, i) => (
+                        <ListItem                    
+                          title={item.emailAddress + " #"+(i+1)}
+                          titleStyle={{textAlign:'center', fontSize:18, fontWeight:400}}
+                          subtitle={item.value + "Units of energy"}
+                          subtitleStyle={{textAlign:'center', fontSize:18, fontWeight:300}}
+                          containerStyle={{backgroundColor:'#ccfaff'}}
+                          bottomDivider
+                          chevron
+                  
+                        />
+                      ))
+                    }
                     </Card>
-
-
-
-
-
-
-
-
-                        <Text>You Vs. House</Text>
-                        <PureChart data={this.state.comparison} type='pie'/>
                     </View>
                     
-                    
-                    
-                    }
-
-               
-                </View>
-           
+                        <Text style={{textAlign:'center', fontSize:20, fontWeight:'500'}}>You Vs. House</Text>
+                        <PureChart data={this.state.comparison} type='pie'/>
+                    </View>                   
+                    }             
+                </View>         
         )
     }
 } 
